d185f4ebd7ab1cdee68ee5d4a340db2c4260d58d
Vito Di Benedetto <vito@fnal.gov>
2016-09-15 12:43:11 -0500

    removed launch of experiment code from bin/workernode_script.sh

--- bin/workernode_script.sh
+++ bin/workernode_script.sh
@@ -42,9 +42,7 @@ eval $(ups list -aK+ dunetpc -z localProducts_* | awk '{if ( $1 ~ "dunetpc" ) {p
 
 ups active
 
-ifdh cp /pnfs/dune/persistent/users/vito/ci_tests_inputfiles/AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
-
-lar --rethrow-all -n 1 -o AntiMuonCutEvents_LSU_v2_dune35t_Current_geant_default.root --config standard_g4_dune35t.fcl AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
+sh ${CONDOR_DIR_INPUT}/experiment_script.sh
 report_exitcode=$?
 
 ls -lh
