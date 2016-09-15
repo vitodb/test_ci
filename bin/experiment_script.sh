f0eefea2b967e1d42265418e07f578fdb44e623c
Vito Di Benedetto <vito@fnal.gov>
2016-09-15 12:42:02 -0500

    added bin/experiment_script.sh script. This will run the experiment code

---
+++ bin/experiment_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ups active
+
+# copy input data
+#------------------
+
+ifdh cp /pnfs/dune/persistent/users/vito/ci_tests_inputfiles/AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
+
+# run experiment code on input data
+#----------------------------------
+
+lar --rethrow-all -n 1 -o AntiMuonCutEvents_LSU_v2_dune35t_Current_geant_default.root --config standard_g4_dune35t.fcl AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
+report_exitcode=$?
+
+ls -lh
