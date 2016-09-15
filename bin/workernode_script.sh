03819c60b0a061df27af55e65d04a18ef517182b
Vito Di Benedetto <vito@fnal.gov>
2016-09-15 12:22:10 -0500

    get exit code from the job

--- bin/workernode_script.sh
+++ bin/workernode_script.sh
@@ -45,6 +45,7 @@ ups active
 ifdh cp /pnfs/dune/persistent/users/vito/ci_tests_inputfiles/AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
 
 lar --rethrow-all -n 1 -o AntiMuonCutEvents_LSU_v2_dune35t_Current_geant_default.root --config standard_g4_dune35t.fcl AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
+report_exitcode=$?
 
 ls -lh
 
