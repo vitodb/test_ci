72299c1a63577f146bb9bff155d340d23fcfcdb9
Vito Di Benedetto <vito@fnal.gov>
2016-09-20 17:43:57 -0500

    the last job of the dag reports the phase status

--- bin/workernode_script.sh
+++ bin/workernode_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ${LOCAL_WORK_DIR}/setup_workernode.sh
+source ${CONDOR_DIR_INPUT}/setup_workernode.sh
 
 
 echo -e "\n\n#######\n\n"
@@ -22,32 +22,41 @@ curl  $curl_extra -v -o tr.out "$build_db_uri/build/add_test_result?fullname=${r
 
 
 echo CONDOR_DIR_INPUT: ${CONDOR_DIR_INPUT}
-echo LOCAL_WORK_DIR: ${LOCAL_WORK_DIR}
 echo INPUT_TAR_FILE: ${INPUT_TAR_FILE}
 echo PWD: ${PWD}
 
 ls -lh ${CONDOR_DIR_INPUT}
 ls -lh ${INPUT_TAR_FILE}
 
-unset CETPKG_BUILD
-unset OLD_MRB_BUILDDIR
-unset PRODUCTS
+
+case "$1" in
+    end)
+        report_exitcode=0
+        curl  $curl_extra -v -o ep.out "$build_db_uri/build/end_phase?fullname=${report_fullname}&phase=${report_phase}&exitcode=${report_exitcode}&platform=${report_platform}&hostname=${report_hostname}&buildtype=${report_buildtype}"
+        ;;
+    *)
+        unset CETPKG_BUILD
+        unset OLD_MRB_BUILDDIR
+        unset PRODUCTS
 
 
-sed -i.orig "s#setenv MRB_TOP .*#setenv MRB_TOP \"$PWD\"# ; s#setenv MRB_SOURCE .*#setenv MRB_SOURCE \"$PWD\"#" localProducts_*/setup
+        sed -i.orig "s#setenv MRB_TOP .*#setenv MRB_TOP \"$PWD\"# ; s#setenv MRB_SOURCE .*#setenv MRB_SOURCE \"$PWD\"#" localProducts_*/setup
 
-source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || source /grid/fermiapp/products/dune/setup_dune.sh
-source ${PWD}/localProducts_*/setup
+        source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh || source /grid/fermiapp/products/dune/setup_dune.sh
+        source ${PWD}/localProducts_*/setup
 
-eval $(ups list -aK+ dunetpc -z localProducts_* | awk '{if ( $1 ~ "dunetpc" ) {print "setup "$1" "$2" -q "$4} }')
+        eval $(ups list -aK+ dunetpc -z localProducts_* | awk '{if ( $1 ~ "dunetpc" ) {print "setup "$1" "$2" -q "$4} }')
 
-ups active
+        ups active
 
-sh ${LOCAL_WORK_DIR}/experiment_script.sh
-report_exitcode=$?
+        sh ${CONDOR_DIR_INPUT}/experiment_script.sh
+        report_exitcode=$?
 
-ls -lh
+        ls -lh
 
-curl  $curl_extra -v -o tr.out "$build_db_uri/build/add_test_result?fullname=${report_fullname}&serverurl=${report_serverurl}&phase=${report_phase}&testname=${CLUSTER}_${PROCESS}&statisticname="status"&platform=${report_platform}&buildtype=${report_buildtype}&hostname=${report_hostname}&value=${report_exitcode}.0"
+        curl  $curl_extra -v -o tr.out "$build_db_uri/build/add_test_result?fullname=${report_fullname}&serverurl=${report_serverurl}&phase=${report_phase}&testname=${CLUSTER}_${PROCESS}&statisticname="status"&platform=${report_platform}&buildtype=${report_buildtype}&hostname=${report_hostname}&value=${report_exitcode}.0"
 
-curl  $curl_extra -v -o ep.out "$build_db_uri/build/end_phase?fullname=${report_fullname}&phase=${report_phase}&exitcode=${report_exitcode}&platform=${report_platform}&hostname=${report_hostname}&buildtype=${report_buildtype}"
+        ;;
+esac
+
+#exit 0
