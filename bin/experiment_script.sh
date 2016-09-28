d06366cb6cd36fc354971dd17188caf92d9bdbcf
Vito Di Benedetto <vito@fnal.gov>
2016-09-28 15:14:39 -0500

    added more config pars in the grid cfg file

--- bin/experiment_script.sh
+++ bin/experiment_script.sh
@@ -11,8 +11,11 @@ ifdh cp /pnfs/dune/persistent/users/vito/ci_tests_inputfiles/AntiMuonCutEvents_L
 #----------------------------------
 
 echo "1: $1"
+shift
 echo "2: $2"
+shift
 echo "@: $@"
+echo "@: ${@//#/ }"
 
 lar --rethrow-all -n 1 -o AntiMuonCutEvents_LSU_v2_dune35t_Current_geant_default.root --config standard_g4_dune35t.fcl AntiMuonCutEvents_LSU_v2_dune35t_Reference_gen_default.root
 report_exitcode=$?
