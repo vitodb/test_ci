cdb6025dd631f25a3630025573f89ae8979863f3
Michele Fattoruso <mfattoru@fnal.gov>
2016-11-10 14:20:13 -0600

    added a rename file phase and script

---
+++ bin/rename_reference.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "starting script"
+#for file in `ifdh ls /pnfs/dune/scratch/users/mfattoru/temporary 2>/dev/null | grep -E "[0-9]{8}"`; do #the timestamp should be at least 8 digit long,AAAAMMDD
+for file in `ifdh ls /pnfs/dune/scratch/users/mfattoru/temporary 2>/dev/null | grep "${build_timestamp}"`; do #the timestamp should be at least 8 digit long,AAAAMMDD
+    echo "current file: $file"
+    filename=${file}
+    #renamed_filename=`echo ${file}| sed 's/[0-9]\{8,14\}//g'`
+    renamed_filename=`echo ${file}| sed "s/${build_timestamp}//g"`
+    echo "renamed file: ${renamed_filename}"
+    temp_status=`ifdh ls ${renamed_filename} 2>/dev/null`
+    echo "###THIS IS THE STATUS OF THE FILE: $temp_status"
+    if [ -n "$temp_status" ];then #the file exist
+        echo "The file ${renamed_filename} already existed.deleting it"
+        ifdh rm -rf ${renamed_filename}
+    fi
+    echo "renaming the file"
+    ifdh rename ${filename} ${renamed_filename}
+
+done
+echo "all the files have been renamed"
