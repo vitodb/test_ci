be167b8e2df705932aa3b5da988eeb3870e5a7b3
Michele Fattoruso <mfattoru@fnal.gov>
2016-11-10 16:37:55 -0600

    update the rename_reference script to automatically find the configuration file to get the reference folder and better configured the script to be modular

--- bin/rename_reference.sh
+++ bin/rename_reference.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
 
-echo -e "\n\nstarting script"
-#for file in `ifdh ls /pnfs/dune/scratch/users/mfattoru/temporary 2>/dev/null | grep -E "[0-9]{8}"`; do #the timestamp should be at least 8 digit long,AAAAMMDD
-for file in `ifdh ls /pnfs/dune/scratch/users/mfattoru/temporary 2>/dev/null | grep "${build_timestamp}"`; do #the timestamp should be at least 8 digit long,AAAAMMDD
-    echo "current file: $file"
-    filename=${file}
-    #renamed_filename=`echo ${file}| sed 's/[0-9]\{8,14\}//g'`
-    renamed_filename=`echo ${file}| sed "s/${build_timestamp}//g"`
-    echo "renamed file: ${renamed_filename}"
-    temp_status=`ifdh ls ${renamed_filename} 2>/dev/null`
-    echo "###THIS IS THE STATUS OF THE FILE: $temp_status"
-    if [ -n "$temp_status" ];then #the file exist
-        echo "The file ${renamed_filename} already existed.deleting it"
-        ifdh rm ${renamed_filename} force
-    fi
-    echo -e "renaming the file \n \n"
-    ifdh rename ${filename} ${renamed_filename}
+get_reference_path(){
+    for path in `env | grep "_DIR" | sed -e "s/.*=//g"`;do
+        fname="${path}/test/ci_tests.cfg"
+        if [ -f "${fname}" ];then
+            echo "configuration file found in the following folder: ${fname}"
+            echo `cat $fname | grep "^INPUTFILEDIR_EXPERIMENT" | sed -e "s/.*=//g"`
+            break
+        fi
+    done
+}
 
-done
-echo "all the files have been renamed"
+rename_reference_files(){
+    reference_path=get_reference_path
+    echo "Reference folder for the update process: $reference_path"
+    #for file in `ifdh ls /pnfs/dune/scratch/users/mfattoru/temporary 2>/dev/null | grep -E "[0-9]{8}"`; do #the timestamp should be at least 8 digit long,AAAAMMDD
+    for file in `ifdh ls ${reference_path}/temporary 2>/dev/null | grep "${build_timestamp}"`; do
+        echo "current file: $file"
+        filename=${file}
+        #renamed_filename=`echo ${file}| sed 's/[0-9]\{8,14\}//g'`
+        renamed_filename=`echo ${file}| sed "s/${build_timestamp}//g"`
+        echo "renamed file: ${renamed_filename}"
+        temp_status=`ifdh ls ${renamed_filename} 2>/dev/null`
+        echo "###THIS IS THE STATUS OF THE FILE: $temp_status"
+        if [ -n "$temp_status" ];then #the file exist
+            echo "The file ${renamed_filename} already existed.deleting it"
+            ifdh rm ${renamed_filename}
+        fi
+        echo -e "renaming the file\n"
+        ifdh rename ${filename} ${renamed_filename}
+
+    done
+    echo "all the files have been renamed"
+}
+
+echo "############### Renaming Updated Reference Files ###############"
+rename_reference_files
