d0ed7c062dd0800ba67fd4c3bc1881d4bc25e7a9
Michele Fattoruso <mfattoru@fnal.gov>
2016-12-05 23:00:00 -0600

    typo

--- bin/ci_regression_test_dunetpc.sh
+++ bin/ci_regression_test_dunetpc.sh
@@ -218,9 +218,9 @@ do
     #if [ ${CHECK_NEW_REFERENCE} == true ];then
         #reference_file=$(echo "${current_file%`echo ${build_platform}`*}${build_platform}${build_identifier}${current_file#*`echo ${build_platform}`}")
         #reference_file=$(echo "${current_file%default*}default${build_platform}${build_identifier}${current_file#*default}")
-        #reference_file=$(echo "${current_file%default*}default${build_platform}${current_file#*default}")
+        reference_file=$(echo "${current_file%default*}default${build_identifier}${current_file#*default}")
         #reference_file="${reference_file//Current/Reference}"
-        reference_file="${current_file//Current/Reference}"
+        reference_file="${reference_file//Current/Reference}"
     #else
         #reference_file="${current_file//Current/Reference}"
     #fi
