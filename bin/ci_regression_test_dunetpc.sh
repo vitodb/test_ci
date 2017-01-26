2095492df0ac49ec53ba40a563245d59b7ad49bc
Michele Fattoruso <mfattoru@fnal.gov>
2017-01-26 15:50:08 -0600

    debug

--- bin/ci_regression_test_dunetpc.sh
+++ bin/ci_regression_test_dunetpc.sh
@@ -347,9 +347,11 @@ function exitstatus
         if [ -n "$ERRORSTRING" ];then
             echo "`basename $PWD`@${EXITSTATUS}@$ERRORSTRING" >> $WORKSPACE/data_production_stats.log
         fi
-        if [[ $UPLOAD_REFERENCE_FILE == true ]];then\
+        if [[ $UPLOAD_REFERENCE_FILE == true ]];then
+            echo "~~~exceptional exit to zero"
             exit 0 #if we failed to fetch the reference file,pretend that everything is ok,so we can generate them and then upload
         else
+            echo "exit in normal exit status"
             exit "${EXITSTATUS}"
         fi
     fi
