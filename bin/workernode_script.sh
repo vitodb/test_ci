e45bf2861c107f0a63ed58035877b0443f537124
Vito Di Benedetto <vito@fnal.gov>
2017-02-09 15:48:14 -0600

    better printout

--- bin/workernode_script.sh
+++ bin/workernode_script.sh
@@ -82,27 +82,14 @@ standard() {
 
 
     echo "run exp code..."
-    echo CMD: $(eval echo \$executable_${EXP_STAGE}) $(eval echo \$arguments_${EXP_STAGE} -c \$FHiCL_${EXP_STAGE} -n \$nevents_per_job_${EXP_STAGE} -o \$output_filename_${EXP_STAGE}) $new_input_filename
-    ### /usr/bin/time -o ${EXP_STAGE}_${CI_PROCESS}.stats_tmp -f "\n${EXP_STAGE} stats\nelapsed_time: %e s\nCPU: %P\nMax_RSS_Mem: %M kb\n" $(eval echo \$executable_${EXP_STAGE}) $(eval echo \$arguments_${EXP_STAGE} -c \$FHiCL_${EXP_STAGE} -n \$nevents_per_job_${EXP_STAGE} -o \$output_filename_${EXP_STAGE}) $new_input_filename
+    ### echo CMD: $(eval echo \$executable_${EXP_STAGE}) $(eval echo \$arguments_${EXP_STAGE} -c \$FHiCL_${EXP_STAGE} -n \$nevents_per_job_${EXP_STAGE} -o \$output_filename_${EXP_STAGE}) $new_input_filename
 
-    echo "ci_exp_cmd: ${ci_exp_cmd}"
+    echo "ci_exp_cmd: $(eval echo ${ci_exp_cmd})"
     echo CMD: $(eval echo ${ci_exp_cmd}) $(eval echo \$arguments_${EXP_STAGE} -c \$FHiCL_${EXP_STAGE} -n \$nevents_per_job_${EXP_STAGE} -o \$output_filename_${EXP_STAGE}) $new_input_filename
     eval $(eval echo ${ci_exp_cmd}) $(eval echo \$arguments_${EXP_STAGE} -c \$FHiCL_${EXP_STAGE} -n \$nevents_per_job_${EXP_STAGE} -o \$output_filename_${EXP_STAGE}) $new_input_filename
 
     report_exitcode=$?
     echo "exitstatus executable: $report_exitcode"
-
-    ### echo "exitstatus_exp_code: $report_exitcode" > ${EXP_STAGE}_${CI_PROCESS}.stats
-    ### timever=$(rpm -qf /usr/bin/time)
-    ### echo ${timever} >> ${EXP_STAGE}_${CI_PROCESS}.stats
-    ### if [[ "$timever" = "time-1.7-37"* || "$timever" = "time-1.7-27"* ]]; then mem_scale=4; else mem_scale=1; fi
-    ### awk '/Max_RSS_Mem:/ {$2=$2/'${mem_scale}'}1' ${EXP_STAGE}_${CI_PROCESS}.stats_tmp >> ${EXP_STAGE}_${CI_PROCESS}.stats
-    ###
-    ### if [ -f  $(eval echo \$output_filename_${EXP_STAGE}) ]; then
-    ###     stat -c "file_size: %s b %n" $(eval echo \$output_filename_${EXP_STAGE}) >> ${EXP_STAGE}_${CI_PROCESS}.stats
-    ### else
-    ###     echo "file_size: 0 b none" >> ${EXP_STAGE}_${CI_PROCESS}.stats
-    ### fi
 
     ls -lh
 
